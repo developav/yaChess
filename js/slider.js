963a7f9672d2e9170b0a4fb6df0cc1a3297bc1d1
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2024-02-05 16:54:47 +0300

    add slider

--- js/slider.js
+++ js/slider.js
@@ -1,44 +1,53 @@
-let slideIndex = 1;
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  direction: 'horizontal',
+  loop: true,
 
-function showSlide(index) {
-  const slides = document.querySelectorAll('.slide');
-  const counter = document.querySelector('.counter');
+  autoplay: {
+    delay: 5000,
+  },
 
-  if (index < 0) {
-        slideIndex = slides.length - 1;
-      } else if (index >= slides.length) {
-        slideIndex = 0;
-      } else {
-        slideIndex = index;
-      }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.transform = `translateX(-${100 * (slideIndex - 1)}%)`;
-  }
+  breakpoints:{
+    1200: {
+      slidesPerView:3,
+      spaceBetween:50,
+    },
+    990: {
+      slidesPerView:3,
+      spaceBetween:50,
+    },
+    680: {
+      slidesPerView:2,
+      spaceBetween:30,
+    },
+    320: {
+      slidesPerView:1,
+      spaceBetween:0,
+    },
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed: 1700,
 
-  counter.textContent = `${slideIndex} / ${slides.length}`;
-}
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    clickable: 'false',
+    totalClass: 'swiper-pagination-total',
+  },
 
-function nextSlideAuto() {
-  showSlide(slideIndex += 1);
-}
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-function startAutoSlide() {
-  setInterval(nextSlideAuto, 4000); // Вызываем функцию переключения слайдов каждые 5 секунд
-}
-startAutoSlide(); // Запуск автоматического листания при загрузке страницы
-
-
-function prevSlide() {
-  showSlide(slideIndex -= 1);
-}
-
-function nextSlide() {
-  showSlide(slideIndex += 1);
-}
-
-document.querySelector('.prev-btn').addEventListener('click', prevSlide);
-document.querySelector('.next-btn').addEventListener('click', nextSlide);
-
-showSlide(slideIndex);
-
-  
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+ 
+});
